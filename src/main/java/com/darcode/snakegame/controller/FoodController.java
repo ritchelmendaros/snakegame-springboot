d776a53a8a96dd220d8c25083c98d9d1a1686609
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-04-28 02:40:38 +0800

    updates food color

--- src/main/java/com/darcode/snakegame/controller/FoodController.java
+++ src/main/java/com/darcode/snakegame/controller/FoodController.java
@@ -1,5 +1,32 @@
 package com.darcode.snakegame.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.darcode.snakegame.model.Food;
+import com.darcode.snakegame.service.FoodService;
+
+@RestController
+@RequestMapping("/food")
 public class FoodController {
-    
+
+    private final FoodService foodService;
+
+    @Autowired
+    public FoodController(FoodService foodService) {
+        this.foodService = foodService;
+    }
+
+    @PutMapping("/updateColor")
+    public ResponseEntity<?> updateFoodColor(@RequestBody Food food) {
+        try {
+            foodService.updateFoodColor(food.getUserid(), food.getFoodcolor());
+            return ResponseEntity.ok("Food color updated successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error updating food color: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/darcode/snakegame/service/FoodService.java
+++ src/main/java/com/darcode/snakegame/service/FoodService.java
@@ -1,5 +1,25 @@
 package com.darcode.snakegame.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.darcode.snakegame.model.Food;
+import com.darcode.snakegame.repository.FoodRepository;
+
+@Service
 public class FoodService {
-    
+
+    private final FoodRepository foodRepository;
+
+    @Autowired
+    public FoodService(FoodRepository foodRepository) {
+        this.foodRepository = foodRepository;
+    }
+
+    public void updateFoodColor(Long foodId, String foodColor) {
+        Food food = foodRepository.findById(foodId)
+                .orElseThrow(() -> new RuntimeException("Food not found with id: " + foodId));
+        food.setFoodcolor(foodColor);
+        foodRepository.save(food);
+    }
 }
